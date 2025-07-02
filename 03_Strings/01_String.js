75eca3e8102507e9ab851cb4580741828505d08a
Deep Kansara <deepkansara1212@gmail.com>
2025-07-02 11:03:14 +0530

    Strings

---
+++ 03_Strings/01_String.js
@@ -0,0 +1,13 @@
+let greet = "Hello World !!!"
+let name = 'Alice'
+let age = 23
+
+// Hello World, "John"    
+// \"  \"  
+
+let str1 = "Hello World, \"John\""
+let str = "Hello World, \'John\'"
+// console.log(str);
+
+
+console.log(`Hello, My name is ${name} and I am ${age} years old.`);
---
+++ 03_Strings/02_String-Methods.js
@@ -0,0 +1,46 @@
+let fname = "Hello World, I am learning Javascript, Javascript is scripting language !!!"
+let lname = "Doe"
+
+// console.log(fname.length);
+
+// console.log(fname.at(2));
+// console.log(fname.charAt(2));
+
+
+// Check whether the original fname is changed or not
+// console.log(fname.concat(lname));
+
+// Check for the startsWith method
+// console.log(fname.endsWith('h'));
+
+// console.log(fname.includes("Javascript"));.
+
+
+// Jaate
+// console.log(fname.lastIndexOf());
+
+
+let age = "23"
+// console.log(age.padEnd(5, "$"));
+// Jaate - padStart()
+
+
+// console.log(fname.replace("Javascript", "Python"));
+// Jaate - replaceAll()
+
+
+// Jaate
+// console.log(fname.slice());
+
+
+let num = "    23     "
+console.log(num.length);
+
+
+let trim_num = num.trim()
+console.log(trim_num.length);
+
+
+// Jaate -> trimEnd()  & trimStart()
+
+
