154a3a2be5b17e55c141bb1b02e70c099f1b1d88
Deep Kansara <deepkansara1212@gmail.com>
2025-07-03 11:19:59 +0530

    Numbers

---
+++ 04_Numbers/01_Number.js
@@ -0,0 +1,13 @@
+const num = 12.4562237
+
+// console.log(num.toExponential(2));
+
+// console.log(num.toFixed(4));/
+
+console.log(num.toPrecision(2));
+
+// console.log(num.toString(2));
+
+
+
+
---
+++ 05_Math/01_Math.js
@@ -0,0 +1,24 @@
+
+// PROPERTIES
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.SQRT2);
+
+
+// METHODS
+console.log(Math.abs(-45210));
+
+console.log(Math.cbrt(8));
+
+console.log(Math.sqrt(16));
+
+console.log(Math.round(4.8512));
+
+console.log(Math.floor(8.1245));
+console.log(Math.ceil(8.1245));
+
+
+console.log(Math.trunc(8.9245));
+
+console.log(Math.pow(3, 10));
+
