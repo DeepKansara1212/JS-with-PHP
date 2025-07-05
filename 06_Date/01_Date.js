b4a0fe9e190278dd5aea27898d86bf9d3d8e2559
Deep Kansara <deepkansara1212@gmail.com>
2025-07-05 11:07:38 +0530

    Date Object

---
+++ 06_Date/01_Date.js
@@ -0,0 +1,13 @@
+const date = new Date()
+
+// console.log(date);
+
+console.log(date.getFullYear());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMonth());
+
+
+date.setFullYear('1925')
+console.log(date.getFullYear()); 
