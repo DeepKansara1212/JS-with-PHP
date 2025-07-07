691de21a4e77bde4bea358f95aaa973cd3cb13b5
Deep Kansara <deepkansara1212@gmail.com>
2025-07-07 10:58:53 +0530

    Array

---
+++ 08_Array/01_Array.js
@@ -0,0 +1,14 @@
+const arr = ["str", 12, true, [], null, undefined]
+
+console.log(arr[2]); 
+
+
+arr[2] = false
+
+console.log(arr);
+
+
+
+// const number = 12
+// number = number + 1
+// console.log(number);
---
+++ 08_Array/02_Array-Methods.js
@@ -0,0 +1,24 @@
+const numbers = [10, 20, 30, 40, 50]
+
+// console.log(numbers.at());
+// console.log(numbers.concat());
+
+
+// console.log(numbers.includes());
+// console.log(numbers.lastIndexOf());
+// console.log(numbers.length());
+// console.log(numbers.pop());
+// console.log(numbers.push("12"));
+// console.log(numbers.reverse());
+// console.log(numbers.shift());
+// console.log(numbers.unshift());
+// console.log(numbers.slice());
+// console.log(numbers.sort());
+// console.log(numbers.toLocaleString());
+
+
+const fruits = ["Mango", "Banana", "Apple"]
+
+fruits.splice(0, 2, "Kiwi")
+console.log(fruits);
+
