6dab90a3e91c272b6d059f46f4471c2ace90f5bd
Deep Kansara <deepkansara1212@gmail.com>
2025-07-08 11:31:50 +0530

    Conditional

---
+++ 09_Conditionals/01_If-Else.js
@@ -0,0 +1,14 @@
+let age = 15
+
+if(age >= 18) {
+    console.log("Valid Voter");  
+}
+
+else if(age >= 60) {
+    console.log("You are senior citizen");  
+}
+
+else {
+    console.log("Invalid");
+}
+
