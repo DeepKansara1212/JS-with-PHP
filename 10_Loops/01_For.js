ee2c61c3a07c9fe29b5f3a30c219b4762ecf8b46
Deep Kansara <deepkansara1212@gmail.com>
2025-07-08 11:31:58 +0530

    Loops

---
+++ 10_Loops/01_For.js
@@ -0,0 +1,23 @@
+
+// For Loop
+for(i = 1; i <= 10; i++) {
+    // console.log("Hello World!!!");
+} 
+
+
+
+// While Loop
+let num = 5
+while(num <= 10) {
+    console.log("Number is less than 5.");
+    num++
+}
+
+
+
+// Do While Loop
+let number = 8
+do {
+    console.log("Number is higher");
+    number++
+} while (number <= 5);
