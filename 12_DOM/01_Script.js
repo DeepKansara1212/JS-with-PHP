fa50f0d9944a33fa5776183b699c89eb6ae723f4
Deep Kansara <deepkansara1212@gmail.com>
2025-07-11 11:04:49 -0700

    DOM Manipulation

---
+++ 12_DOM/01_Script.js
@@ -0,0 +1,9 @@
+// Selection of elements in the DOM
+const heading1 = document.getElementById("heading-1");
+const para1 = document.getElementsByClassName("para1")
+const para2 = document.querySelector(".container") 
+const para = document.querySelectorAll(".para1")
+
+
+para.style.color = "blue";
+
