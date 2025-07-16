ee0a528928d521e5272b9e33ce74c4b225cb6206
Deep Kansara <deepkansara1212@gmail.com>
2025-07-16 11:04:16 +0530

    DOM Manipulation

--- 12_DOM/01_Script.js
+++ 12_DOM/01_Script.js
@@ -3,7 +3,26 @@ const heading1 = document.getElementById("heading-1");
 const para1 = document.getElementsByClassName("para1")
 const para2 = document.querySelector(".container") 
 const para = document.querySelectorAll(".para1")
+// const tag = document.getElementsByTagName(p)
 
 
-para.style.color = "blue";
+// para.style.color = "blue";
 
+
+const googleLink = document.getElementById('google-link') 
+const googleAtr = googleLink.getAttribute('href')
+
+googleLink.setAttribute('href', 'https://google.com')
+
+// console.log(googleAtr);
+
+
+
+document.querySelector('#p1') 
+
+
+const p1 = document.querySelector('#p1')
+p1.innerHTML = "<h4>Hello Javascript</h4>"
+
+p1.style.backgroundColor = "blue"
+p1.style.color = "white"
